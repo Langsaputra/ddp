6faac13b02ef2bec048e8a33eaa344fed8bc0050
langschool99@gmail.com <langschool99@gmail.com>
2023-10-30 22:10:54 +0700

    push ddp

---
+++ tugas2.py
@@ -0,0 +1,29 @@
+print ("Nama  = Galang Saputra")
+print ("NIM = 0110223036")
+print ("Kelas = TI01")
+print ("Alamat = JL.H.Sanip 4")
+
+print()
+
+print("Nama: Muhammad Wildan Ziyad Alfarabi")
+print("Nim : 0110223003")
+print(" Kelas : TI01")
+print("Jln irian jaya.depok.jaya")
+
+print()
+
+print(9+5)
+
+print()
+
+print(7-4)
+
+print()
+
+print(5*5)
+
+print()
+
+print(10/2)
+
+print()
